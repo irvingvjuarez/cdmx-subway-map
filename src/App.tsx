f10e875f1015acdb46ba8e090a09de69564b480d
Irving Juárez <irvingjuarez274@gmail.com>
2022-08-23 23:11:56 -0500

    styled-components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
 import OlMap from "./components/Map"
+import styled from 'styled-components'
+
+const AppStyles = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
 
 const App = () => (
-  <div className="App">
+  <AppStyles>
     <OlMap />
-  </div>
+  </AppStyles>
 )
 
 export default App
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,10 +1,16 @@
 import { useMap } from "@app/hooks/useMap"
+import styled from 'styled-components'
+
+const MapStyles = styled.div`
+  width: 100%;
+  height: 100%;
+`
 
 const OlMap = () => {
   const { mapRef } = useMap()
 
   return(
-    <div className="map-container" ref={mapRef}></div>
+    <MapStyles ref={mapRef}></MapStyles>
   )
 }
 
