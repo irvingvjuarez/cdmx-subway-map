d82fabf3d062785c963c555246dd08132a99e4b7
Irving Juárez <irvingjuarez274@gmail.com>
2022-08-25 20:47:34 -0500

    Map restored to what it was at the very beginning

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,11 +1,5 @@
 import { useMap } from "@app/hooks/useMap"
-import { useEffect } from "react"
 import styled from 'styled-components'
-import proj4 from "proj4"
-
-// todo: register not imported from ol/proj
-import { get as getProjection, register } from "ol/proj"
-import { View } from "ol"
 
 const MapStyles = styled.div`
   width: 100%;
@@ -13,24 +7,7 @@ const MapStyles = styled.div`
 `
 
 const OlMap = () => {
-  const { mapRef, map } = useMap()
-
-  useEffect(() => {
-    proj4.defs('EPSG:27700', '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 ' +
-      '+x_0=400000 +y_0=-100000 +ellps=airy ' +
-      '+towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 ' +
-      '+units=m +no_defs'
-    );
-    register(proj4);
-    const proj27700 = getProjection('EPSG:27700');
-    proj27700?.setExtent([0, 0, 700000, 1300000])
-
-    map.setView(new View({
-      projection: 'EPSG:27700',
-      center: [400000, 6500000],
-      zoom: 4
-    }))
-  }, [])
+  const { mapRef } = useMap()
 
   return(
     <MapStyles ref={mapRef}></MapStyles>
